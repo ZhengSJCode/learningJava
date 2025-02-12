be9b0a8e810a0812884c52225b0f58b825e95d00
zhengshuaijie <352326430@qq.com>
2025-02-12 16:51:21 +0800

    jni

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
+    static {
+        System.out.println("static block");
+        System.loadLibrary("Main");
+    }
+    
+    private native void sayHello();
 
+    public static void main(String[] args) {
+        System.out.println("main method");
+        new Main().sayHello();
     }
 }
 
