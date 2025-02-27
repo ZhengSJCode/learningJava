c98599f18e90b686ca7a64e053ff3d2de09d6570
zhengshuaijie <352326430@qq.com>
2025-02-27 16:11:19 +0800

    javabytecode

--- HelloByteCode.java
+++ HelloByteCode.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class HelloByteCode {
     public static void main(String[] args) {
+//        System.getProperties().list(System.out);
+        System.getProperty("os");
+        System.out.println(args.length);
+        HashMap map = new HashMap();
+        for (int i = 0; i < 100000000; i++) {
+            map.put(i,1 + i);
+//            System.out.println(args[i]);
+        }
+//        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor();
         HelloByteCode obj = new HelloByteCode();
     }
 }
